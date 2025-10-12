eb4a79faaa573f343878cb795b87028340bf6116
Ravi Shetty <ravishetty05082@gmail.com>
2025-10-12 09:46:39 +0530

    UI Fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
+                { text: "+91 99XXX99XXX", path: '/', icon: MailIcon },
                 { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "225, Bangalore", path: '/', icon: MapPinIcon }
             ]
         }
     ];
